96ab6177d22a62ac5a675002deee08922c9b289f
Zayyed <zayyedmansoor@gmail.com>
2020-09-27 17:31:56 -0400

    final styling and bug fixes

--- scripts.js
+++ scripts.js
@@ -1,43 +1,41 @@
+// pokemon data
+let pokemons = [
+  { number: 1, name: "Bulbasaur", type: "Grass" },
+  { number: 2, name: "Ivysaur", type: "Grass" },
+  { number: 3, name: "Venusaur", type: "Grass" },
+  { number: 4, name: "Charmander", type: "Fire" },
+  { number: 5, name: "Charmeleon", type: "Fire" },
+  { number: 6, name: "Charizard", type: "Fire" },
+  { number: 7, name: "Squirtle", type: "Water" },
+  { number: 8, name: "Wartortle", type: "Water" },
+  { number: 9, name: "Blastoise", type: "Water" },
+  { number: 10, name: "Caterpie", type: "Bug" },
+  { number: 11, name: "Metapod", type: "Bug" },
+  { number: 12, name: "Butterfree", type: "Bug" },
+  { number: 13, name: "Weedle", type: "Bug" },
+  { number: 14, name: "Kakuna", type: "Bug" },
+  { number: 15, name: "Beedrill", type: "Bug" },
+  { number: 16, name: "Pidgey", type: "Normal" },
+  { number: 17, name: "Pidgeotto", type: "Normal" },
+  { number: 18, name: "Pidgeot", type: "Normal" },
+  { number: 19, name: "Rattata", type: "Normal" },
+  { number: 20, name: "Raticate", type: "Normal" }
+];
+
 function makeList(critera) {
-  let pokemons = [
-    { number: 1, name: "Bulbasaur", image: "", type: "Grass" },
-    { number: 2, name: "Ivysaur", image: "", type: "Grass" },
-    { number: 3, name: "Venusaur", image: "", type: "Grass" },
-    { number: 4, name: "Charmander", image: "", type: "Fire" },
-    { number: 5, name: "Charmeleon", image: "", type: "Fire" },
-    { number: 6, name: "Charizard", image: "", type: "Fire" },
-    { number: 7, name: "Squirtle", image: "", type: "Water" },
-    { number: 8, name: "Wartortle", image: "", type: "Water" },
-    { number: 9, name: "Blastoise", image: "", type: "Water" },
-    { number: 10, name: "Caterpie", image: "", type: "Bug" },
-    { number: 12, name: "Metapod", image: "", type: "Bug" },
-    { number: 13, name: "Butterfree", image: "", type: "Bug" },
-    { number: 14, name: "Weedle", image: "", type: "Bug" },
-    { number: 15, name: "Kakuna", image: "", type: "Bug" },
-    { number: 16, name: "Beedrill", image: "", type: "Bug" },
-    { number: 17, name: "Pidgey", image: "", type: "Normal" },
-    { number: 18, name: "Pidgeotto", image: "", type: "Normal" },
-    { number: 19, name: "Pidgeot", image: "", type: "Normal" },
-    { number: 20, name: "Rattata", image: "", type: "Normal" },
-  ];
+  
 
   listContainer = document.createElement("div");
 
   listElement = document.createElement("ul");
-  
-  
 
-  if(critera){
-    console.log('yuh')
+  if (critera) {
     listContainer.id = "results";
     document.getElementById("popup").appendChild(listContainer);
-    
-  }
-  else{
+  } else {
     document.getElementById("pokemon").appendChild(listContainer);
   }
 
-  
   listContainer.appendChild(listElement);
 
   results = 0;
@@ -48,19 +46,16 @@ function makeList(critera) {
     let type = `${pokemon["type"]} Type`;
     let imgPath = `../pokemon/${pokemon["number"]}.png`;
 
-    console.log(name, number, type);
-
     listItem = document.createElement("li");
 
-    PokeName = document.createElement("h5");
+    PokeName = document.createElement("h4");
     PokeType = document.createElement("p");
     PokeImg = document.createElement("img");
 
-    PokeName.innerHTML = number + ' ' + name;
+    PokeName.innerHTML = number + " " + name;
     PokeType.innerHTML = type;
     PokeImg.src = imgPath;
 
-    
     listItem.appendChild(PokeName);
     listItem.appendChild(PokeType);
     listItem.appendChild(PokeImg);
@@ -77,28 +72,25 @@ function makeList(critera) {
     } else {
       listElement.appendChild(listItem);
     }
-
   }
 }
 
-// First Render
-makeList("");
-
-// Search Calls
-
+// Search Functions
 function numSearch() {
   let num = document.getElementById("numVal").value;
 
+  //clean up previous results
+  try {
+    document.getElementById("popup").innerHTML = "";
+    document.getElementById("results").remove();
+  } catch (err) {
+    console.log(err);
+  }
+
+  //input validation
   if (num > 20 || num < 1) {
-    alert("Please Select Pokemon 1 - 20");
+    document.getElementById("popup").innerHTML = "Please Select Pokemon 1 - 20";
   } else {
-    console.log(num);
-    try{
-      document.getElementById("results").remove();
-    }
-    catch(err){
-      console.log(err)
-    }
     makeList(num);
   }
 }
@@ -108,17 +100,25 @@ function nameSearch() {
 
   let letters = /^[a-zA-Z]*$/;
 
+  //clean up previous results
+  try {
+    document.getElementById("popup").innerHTML = "";
+    document.getElementById("results").remove();
+  } catch (err) {
+    console.log(err);
+  }
+
   if (!name.match(letters) || name.length > 20 || name == "") {
-    alert("Only Letters and Max Name Character Length is 20 ");
-  } else {
-    console.log(name);
-    try{
-      document.getElementById("results").remove();
-    }
-    catch(err){
-      console.log(err)
-    }
-    
+    document.getElementById("popup").innerHTML = "Only Letters and Max Name Character Length is 20";
+  } 
+  else {
     makeList(name);
   }
 }
+
+
+// First Render
+makeList("");
+
+
+
